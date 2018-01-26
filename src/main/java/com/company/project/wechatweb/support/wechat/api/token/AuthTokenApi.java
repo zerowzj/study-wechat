99846b7bd43bd6057695b1672b9863ae75d6a61a
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2018-01-26 10:52:02 +0800

    更新代码

--- src/main/java/com/company/project/wechatweb/support/wechat/api/token/AuthTokenApi.java
+++ src/main/java/com/company/project/wechatweb/support/wechat/api/token/AuthTokenApi.java
@@ -20,21 +20,18 @@ public class AuthTokenApi {
 
     private static final String URL = "https://api.weixin.qq.com/sns/oauth2/access_token";
 
-    private static final String APP_ID;
+    private static final String GRANT_TYPE = "authorization_code";
 
-    private static final String SECRET;
+    private static final String APP_ID = WechatCfg.getAppId();
+
+    private static final String SECRET = WechatCfg.getSecret();
 
     private static AuthTokenResp token;
 
     private static long overTimeMillis = -1;
 
-    static {
-        APP_ID = WechatCfg.getAppId();
-        SECRET = WechatCfg.getSecret();
-    }
-
     /**
-     * 获取access_token
+     * 获取Access Token
      *
      * @param code
      * @return String
@@ -42,24 +39,22 @@ public class AuthTokenApi {
     public static AuthTokenResp getAccessToken(String code) {
         long now = System.currentTimeMillis();
         if (overTimeMillis <= now) {
-            LOGGER.info("请求获取[access_token]");
+            LOGGER.info("请求获取[access token]");
             doGetToken(code);
         }
         if (token == null) {
-            throw new IllegalStateException("未获取到[access_token]");
+            throw new IllegalStateException("未获取到[access token]");
         }
         return token;
     }
 
     private static void doGetToken(String code) {
         Map<String, String> params = Maps.newHashMap();
-        params.put("grant_type", "authorization_code");
+        params.put("grant_type", GRANT_TYPE);
         params.put("appid", APP_ID);
         params.put("secret", SECRET);
         params.put("code", code);
         HttpRequest request = HttpRequest.get(URL, params, false);
-
-
         if (request.ok()) {
             String body = request.body();
             LOGGER.info(body);
