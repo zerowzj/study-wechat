e83abce5b92f9da732a4a5f68d141a106bcb086e
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2018-01-26 14:24:56 +0800

    更新代码

---
+++ src/main/java/com/company/project/wechatweb/web/WechatRedirectApi.java
@@ -0,0 +1,31 @@
+package com.company.project.wechatweb.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 微信Msg Api
+ *
+ * @author wangzhj
+ */
+@Controller
+public class WechatRedirectApi {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WechatRedirectApi.class);
+
+    private static ApplicationContext cxt;
+
+    @RequestMapping("/wechat/oauth")
+    public void dispatch(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            response.sendRedirect("");
+        } catch (Exception ex) {
+        }
+    }
+}
