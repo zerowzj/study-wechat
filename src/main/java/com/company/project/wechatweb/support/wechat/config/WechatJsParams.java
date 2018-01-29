0416a0d62f7f81548141ea03fba4334d9062032b
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2018-01-29 10:57:51 +0800

    更新代码

---
+++ src/main/java/com/company/project/wechatweb/support/wechat/config/WechatJsParams.java
@@ -0,0 +1,27 @@
+package com.company.project.wechatweb.support.wechat.config;
+
+/**
+ * 微信Js配置
+ *
+ * @author wangzhj
+ */
+public final class WechatJsParams {
+
+    /**
+     * 生成时间戳
+     *
+     * @return String
+     */
+    public static String createTimestamp() {
+        return "";
+    }
+
+    /**
+     * 生成随机串
+     *
+     * @return String
+     */
+    public static String createNonceStr() {
+        return "";
+    }
+}
